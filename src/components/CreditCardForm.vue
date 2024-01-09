77cb4e91c681f06b55bfeb50a63e39d8cb7ab736
Andres Luna <jaime.pibe41@gmail.com>
2024-01-09 15:54:32 -0500

    add form credit card

---
+++ src/components/CreditCardForm.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="max-w-sm mx-auto bg-white rounded-md shadow-md overflow-hidden">
+        <div class="px-6 py-4 bg-gray-900 text-white">
+            <h1 class="text-lg font-bold">Tarjeta de Credito</h1>
+        </div>
+        <div class="px-6 py-4">
+
+            <div class="mb-4">
+                <label class="block text-gray-700 font-bold mb-2" for="card-number">
+                    Numero de Tarjeta
+                </label>
+                <div class="flex flex-row">
+                    <input
+                        class="appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="card-number" type="text" placeholder="**** **** **** ****" v-model="cardNumber" max="19" min="10">
+                    <img :src="`/src/assets/${cardImage}`" :alt="`TC-${cardImage}`" v-if="cardImage != ''" width="150px" height="20px">
+                </div>
+            </div>
+
+            <div class="mb-4">
+                <label class="block text-gray-700 font-bold mb-2" for="expiration-date">
+                    Fecha de Expiracion
+                </label>
+                <input
+                    class="appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="expiration-date" type="text" placeholder="MM/YY">
+            </div>
+
+            <div class="mb-4">
+                <label class="block text-gray-700 font-bold mb-2" for="cvv">
+                    CVV
+                </label>
+                <input
+                    class="appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="cvv" type="text" placeholder="***">
+            </div>
+
+            <div class="mb-4">
+                <label class="block text-gray-700 font-bold mb-2" for="cvv">
+                    Nombre en la tarjeta
+                </label>
+                <input
+                    class="appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    type="text" placeholder="Full Name">
+            </div>
+
+            <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full">
+                Pagar ahora
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+const cardNumber = ref('')
+const cardDateExpiration = ref('')
+const cardCVV = ref('')
+const cardFullName = ref('')
+
+const cardImage = computed(() => {
+    console.log(cardNumber.value);
+    const num = cardNumber.value.replace(/\s/g, '');
+
+    if (/^\d+$/.test(num) && (num.length >= 13 && num.length <= 19)) {
+        if (/^4/.test(num)) {
+            return 'visa.png';
+        } else if (/^5[1-5]/.test(num)) {
+            return 'mastercard.png';
+        } else {
+            return '';
+        }
+    } else {
+        return '';
+    }
+})
+</script>
--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="flex flex-col md:flex-row">
         <div class="md:w-1/2">
-            <img :src="product.imagen" alt="Imagen de {{ product.nombre }}" class="w-full h-auto rounded-lg shadow-lg">
+            <img :src="product.imagen" alt="Imagen de {{ product.nombre }}" class="w-1/2 h-auto rounded-lg shadow-lg">
         </div>
         <div class="md:w-1/2 mt-4 md:mt-0 md:ml-6">
             <h2 class="text-2xl font-bold mb-2">{{ product.nombre }}</h2>
             <p class="text-gray-700 text-lg mb-4">Categoría: {{ product.categoria }}</p>
             <p class="text-gray-700 text-lg mb-4">Precio: ${{ product.precio.toFixed(2) }}</p>
             <p class="text-gray-700 text-base mb-4">{{ product.descripcion }}</p>
-            <Modal :is-modal-visible="isModalVisible" @onToggle="onToggle">
+            <Modal :is-modal-visible="isModalVisible" @onToggle="onToggle" :modal-content="CreditCardForm">
                 <button @click="onToggle"
                     class="bg-blue-500 text-white py-2 px-4 rounded-md shadow-md hover:bg-blue-600">Pagar con Tarjeta de
                     Crédito</button>
@@ -21,15 +21,15 @@
 <script setup>
 import { ref, computed } from "vue";
 import Modal from "../components/Modal.vue";
+import CreditCardForm from "../components/CreditCardForm.vue";
 import { useRoute } from 'vue-router';
 import { useProductStore } from '../store/productStore';
 const route = useRoute();
 const store = useProductStore();
 const product = store.getProduct(route.params.id)
-const isOpen = ref(true)
+const isOpen = ref(false)
 const isModalVisible = computed(() => {
     return isOpen.value;
-
 })
 const onToggle = () => {
     isOpen.value = !isOpen.value;
